c34178c6e50baa27a7d01732e96ecc7cf9681687
julienchriste <julien@aquaverde.ch>
2013-03-11 16:34:05 +0100

    dynamize project title

--- aquarius/config.php
+++ aquarius/config.php
@@ -2,12 +2,10 @@
 
 // Basic Settings
 // -----------------------------------------------------------------------
-define('PROJECT_TITLE', 'aquarius-cms.ch');
+define('PROJECT_TITLE', get($_SERVER, 'SERVER_NAME'));
 
 /* Automagically assign content fields for each item in the smarty {list} block. */
 $config['frontend']['loadcontent'] = true;
 
 // default directory
 defined('DEFAULT_SELECTED_DIR') or define('DEFAULT_SELECTED_DIR', 'pictures/content');
-
-?>
